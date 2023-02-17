191081b4b379ff4a1b4bfb98e4e250caace3cc7d
Homework1 <danisholha760@gmail.com>
2023-02-17 02:12:47 -0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static  void main(String[] args) {
+
+        List<String> names = new ArrayList<>(List.of());
+        names.add("Masha");
+        names.add("Petya");
+        names.add("Ivan");
+        System.out.println(names);
+
+
+        List<String> newNames = new ArrayList<>(List.of());
+        for (String name : names) {
+            String x = name.substring(1);
+            newNames.add(x);
+        }
+        System.out.println(newNames);
+
     }
 }
