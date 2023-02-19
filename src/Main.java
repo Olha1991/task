a17e146a6e8a94f2a3b2a696b97ae6390f03d7dd
Homework1 <danisholha760@gmail.com>
2023-02-18 22:54:49 -0600

    added sorting

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static java.util.Collections.sort;
 
 public class Main {
     public static  void main(String[] args) {
 
-        List<String> names = new ArrayList<>(List.of());
+        List<String> names = new ArrayList<>(3);
         names.add("Masha");
-        names.add("Petya");
         names.add("Ivan");
+        names.add("Petya");
+
         System.out.println(names);
 
 
-        List<String> newNames = new ArrayList<>(List.of());
+        List<String> newNames = new ArrayList<>();
         for (String name : names) {
             String x = name.substring(1);
             newNames.add(x);
+            Collections.sort(newNames);
         }
         System.out.println(newNames);
 
